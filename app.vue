084afd282d83fd242faa66dc5a2ef242b6a42c4f
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2023-10-22 21:18:04 +0200

    rm: comments

--- app.vue
+++ app.vue
@@ -2,13 +2,7 @@
   <div>
     <TheHeader />
     <NuxtLayout>
-      <!-- NuxtPage for routing -->
       <NuxtPage />
     </NuxtLayout>
-    <!-- <Landing />
-    <Components /> 
-    <About />
-    <Projects />
-    <Contact /> -->
   </div>
 </template>
