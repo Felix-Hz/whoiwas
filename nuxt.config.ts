e55de5108b5a64f2201f54c985dd567853be55c2
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2023-11-05 13:49:54 +0100

    fix: mobile overflow & unable touchaction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  // Every time that I use @ it'll resolve to root dir.
-  // alias: {
-  //   "@": resolve(__dirname, "/"),
-  // },
   // devtools: { enabled: true },
-  // image: {
-  //   dir: "public/media",
-  //   quality: 50,
-  // },
   modules: ["nuxt-icon"],
   // Tailwind config
   css: ["~/assets/css/main.css"],
