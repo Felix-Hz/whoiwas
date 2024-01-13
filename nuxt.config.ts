a577b6e1304e4a387f0c55cbffa87e3e05cff99d
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-01-13 17:49:51 +0100

    add: nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  // devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["nuxt-icon", "nuxt-particles"],
   // Tailwind config
   css: ["~/assets/css/main.css"],
