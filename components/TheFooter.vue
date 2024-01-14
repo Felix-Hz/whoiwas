9bb50cff4fbb791c8a3458421012618f62322dbc
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-01-14 12:08:05 +0100

    update: year

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,3 +1,5 @@
 <template>
-  <footer class="text-center p-4 text-gray-400 opacity-60 m-0">© 2023 | Made with ☕️ by Felix Hernandez Vieyra</footer>
+  <footer class="text-center p-4 text-gray-400 opacity-60 m-0">
+    © 2024 | Made with ☕️ by Felix Hernandez Vieyra
+  </footer>
 </template>
