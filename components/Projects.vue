d93df9bae89f86bdf61b7f38582b86586cca4c3e
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-01-15 23:18:48 +0100

    add: project card component container

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,12 +1,3 @@
 <template>
-  <div class="component-container">This is the projects page.</div>
+  <ProjectsContainer />
 </template>
-<style scoped>
-/* Apply styles to the root element of the component */
-.component-container {
-  height: 100vh;
-  margin: auto;
-  width: 50%;
-  color: blue;
-}
-</style>
---
+++ components/projects/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="h-screen z-1 mt-20 container mx-auto">
+    <div class="flex flex-row space-x-8">
+      <ProjectsCard
+        projectName="Dummy Project"
+        projectUrl="https://google.com/"
+        projectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+      />
+      <ProjectsCard
+        projectName="Dummy Project"
+        projectUrl="https://google.com/"
+        projectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+      />
+      <ProjectsCard
+        projectName="Dummy Project"
+        projectUrl="https://google.com/"
+        projectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+      />
+    </div>
+  </main>
+</template>
