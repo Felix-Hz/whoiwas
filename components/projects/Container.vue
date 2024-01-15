e8420936dcfb3f8ccb03769ab89b80a9d2ebd440
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-01-15 23:33:34 +0100

    add: card container title and description

--- components/projects/Container.vue
+++ components/projects/Container.vue
@@ -1,6 +1,20 @@
 <template>
   <main class="h-screen z-1 mt-20 container mx-auto">
-    <div class="flex flex-row space-x-8">
+    <div class="xs:pb-4 xs:mt-24 md:pt-24 lg:pt-8">
+      <h2
+        class="xs:text-3xl text-4xl text-white-900 p-0 mb-4 tracking-tight text-center"
+      >
+        Project Gallery
+      </h2>
+      <p
+        class="xs:hidden mb-8 lg:text-xl lg:mb-12 font-light text-center text-gray-500"
+      >
+        I've been working on some cool pet projects that you can check out
+        below.
+      </p>
+    </div>
+
+    <div class="flex flex-row space-x-16 p-5">
       <ProjectsCard
         projectName="Dummy Project"
         projectUrl="https://google.com/"
