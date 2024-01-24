1b4440be469fb5d2e3adc3ae3cc71afc8a3b969b
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-01-24 18:36:59 +0100

    add: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+    },
+  },
   devtools: { enabled: false },
   modules: ["nuxt-icon"],
   // Tailwind config
