27419d40ad556e27714533fa66c34175e98ca7d2
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-03-05 11:32:28 +0100

    add: footer i18n

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,6 +2,6 @@
   <footer
     class="text-center p-4 text-gray-400 bg-transparent opacity-80 m-auto"
   >
-    <h2 class="inline">© 2024 | Made with ☕️ by Felix Hernandez Vieyra</h2>
+    <h2 class="inline">{{ $t("footer") }}</h2>
   </footer>
 </template>
