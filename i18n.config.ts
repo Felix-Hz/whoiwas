a6d6e65c7eecf4de2fa854b8325ac637d4d3f35a
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-03-05 11:37:52 +0100

    add: toolkit i18n

--- i18n.config.ts
+++ i18n.config.ts
@@ -4,22 +4,31 @@ export default defineI18nConfig(() => ({
   messages: {
     en: {
       welcome: "Welcome",
+      /* Toolkit */
+      title_3: "The Toolkit",
+      subtitle_toolkit:
+        "These are some technologies that I've employed recently for both professional projects, and personal endeavors.",
       /* Contact */
+      title_2: "Let's Chat",
+      form_email: "Your email",
       form_subject: "Subject",
+      form_msg: "Your message",
+      form_submit: "Start the conversation",
+      contact_subtitle:
+        "Whether you have me in mind for a job opportunity, have stumbled upon a bug or want to leave some feedback, I'm all ears.",
       form_placeholder_subject: "What's all of this about?",
       form_placeholder_msg:
         "I'd love to know more about it. Keystroke, keystroke, keystroke...",
-      title_2: "Let's Chat",
-      form_msg: "Your message",
-      form_submit: "Start the conversation",
-      form_email: "Your email",
+      /* Footer */
       footer: "© 2024 - Made with ☕️ by Felix Hernandez Vieyra",
-      contact_subtitle:
-        "Whether you have me in mind for a job opportunity, have stumbled upon a bug or want to leave some feedback, I'm all ears.",
     },
     es: {
       welcome: "Bienvenido",
       title_1: "La Galería",
+      /* Toolkit */
+      title_3: "El Toolkit",
+      subtitle_toolkit:
+        "Estas son algunas de las tecnologías que usé recientemente en proyectos tanto profesionales como personales.",
       /* Contact */
       title_2: "Charlemos",
       form_email: "Email",
