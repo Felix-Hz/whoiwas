94a1275a134a0bb70e641766a3db30fa3cd1c752
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-03-06 06:59:54 +0100

    add: comp IDs smooth scroll

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,9 @@
     <template v-if="loaded">
       <TheHeader @headerHeightChanged="updateHeaderHeight" />
       <section class="custom-dot-background m-4 p-1" ref="loadedPage">
-        <About :headerHeight="headerHeight" />
-        <Projects />
-        <Contact />
+        <About :headerHeight="headerHeight" id="about" />
+        <Projects id="contact" />
+        <Contact id="contact" />
       </section>
     </template>
   </div>
